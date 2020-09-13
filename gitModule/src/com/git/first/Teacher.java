69ed2788bc906842b542fe5f22cef126f0a611d1
yahao Guo <245944369@qq.com>
2020-09-13 21:02:50 +0800

    使用git第一次提交代码

---
+++ gitModule/src/com/git/first/Teacher.java
@@ -0,0 +1,15 @@
+package com.git.first;
+
+/**
+ * @ClassName Teacher
+ * @Description TODO
+ * @Author yahao
+ * @Date 2020/9/13 20:54
+ * @Version 1.0
+ */
+public class Teacher {
+    public static final String FIRSTGIT_NAME = "第一次用git开发";
+    public static void main(String[] args){
+        System.out.println(FIRSTGIT_NAME);
+    }
+}
