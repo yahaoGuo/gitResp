86c384c723cff8babf08872b14504511665e6242
yahao Guo <245944369@qq.com>
2020-09-13 21:42:27 +0800

    使用git第二次提交代码，增加一些属性。

---
+++ gitModule/src/com/git/first/Student.java
@@ -0,0 +1,12 @@
+package com.git.first;
+
+/**
+ * @ClassName Student
+ * @Description TODO
+ * @Author yahao
+ * @Date 2020/9/13 21:41
+ * @Version 1.0
+ */
+public class Student {
+    private String name;
+}
