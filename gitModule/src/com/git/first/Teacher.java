a4e10ab35aa74dfd5b6b78f9a51302be92be8740
yahao Guo <245944369@qq.com>
2020-09-13 21:20:10 +0800

    使用git第二次提交代码，增加一些属性。

--- gitModule/src/com/git/first/Teacher.java
+++ gitModule/src/com/git/first/Teacher.java
@@ -9,7 +9,39 @@ package com.git.first;
  */
 public class Teacher {
     public static final String FIRSTGIT_NAME = "第一次用git开发";
+    private String student;
+    private String name;
+    private String age;
+
+    public String getStudent() {
+        return student;
+    }
+
+    public void setStudent(String student) {
+        this.student = student;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
     public static void main(String[] args){
+        say();
         System.out.println(FIRSTGIT_NAME);
+    }
+    public static void say(){
+        System.out.println("这次提交了一些属性");
     }
 }
