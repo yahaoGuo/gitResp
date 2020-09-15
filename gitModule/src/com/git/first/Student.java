beba9f805947a32b7130f8d891ff2784b62d7c2f
yahao Guo <245944369@qq.com>
2020-09-15 23:21:28 +0800

    修改student类

--- gitModule/src/com/git/first/Student.java
+++ gitModule/src/com/git/first/Student.java
@@ -1,5 +1,7 @@
 package com.git.first;
 
+import java.util.ArrayList;
+
 /**
  * @ClassName Student
  * @Description TODO
@@ -9,4 +11,23 @@ package com.git.first;
  */
 public class Student {
     private String name;
+    public static void main(String[] args){
+        int[] nums = {2,7,11,14};
+        int target = 9;
+        int[] array = new int[2];
+        ArrayList<Integer> list = new ArrayList<Integer>();
+        for(int i=0;i<nums.length;i++){
+            for(int j=nums.length-1;j>i;j--){
+                if(nums[i]+nums[j] == target){
+                    System.out.println(i+"--"+j);
+                    list.add(i);
+                    list.add(j);
+                    array[0] = i;
+                    array[1] = j;
+                    break;
+                }
+            }
+        }
+        System.out.println(list+"数组："+array+"mums:"+nums);
+    }
 }
